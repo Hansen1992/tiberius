4692642a3f46237f2b6a5ab0d6e6bcb8401a56c2
Martin Hansen <noerhaaskov@gmail.com>
2021-05-23 20:08:42 +0200

    tiberius

---
+++ src/script.js
@@ -0,0 +1,113 @@
+import './style.css';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Models
+ */
+let model;
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load('/models/tiberius/scene.gltf', (gltf) => {
+	model = gltf.scene.children[0];
+	const box = new THREE.Box3().setFromObject(model);
+	const center = box.getCenter(new THREE.Vector3());
+
+	gltf.scene.position.x += gltf.scene.position.x - center.x;
+	gltf.scene.position.y += gltf.scene.position.y - center.y;
+	model.position.z = -1.8;
+	scene.add(gltf.scene);
+});
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
+);
+camera.position.set(1, 1, 1);
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+	alpha: true,
+	antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const animate = () => {
+	const elapsedTime = clock.getElapsedTime();
+
+	if (model) {
+		camera.position.x = Math.cos(elapsedTime / 6);
+		camera.lookAt(model.position);
+	}
+
+	// Render
+	renderer.render(scene, camera);
+
+	// Call animate again on the next frame
+	window.requestAnimationFrame(animate);
+};
+
+animate();
